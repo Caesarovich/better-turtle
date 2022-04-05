64f16e8a6d30bc52a15ece303a6590fcfb8fa900
Caesarovich <cesar@caesarovich.xyz>
2022-04-05 17:31:43 +0200

    Update test

--- src/test.ts
+++ src/test.ts
@@ -2,17 +2,26 @@ import { createCanvas } from 'canvas';
 import { Turtle } from './turtle';
 import { createWriteStream } from 'fs';
 import { join } from 'path';
+import { BuiltInShapes } from './shapes';
 
 const outStream = createWriteStream(join(__dirname, '/test.png'));
 const canvas = createCanvas(400, 400);
 const ctx = canvas.getContext('2d');
 
-const turtle = new Turtle(ctx as CanvasRenderingContext2D);
+const turtle = new Turtle(ctx as CanvasRenderingContext2D, {
+  disableWrapping: true,
+  shape: BuiltInShapes.Triangle,
+});
 
 turtle
+  .expose(globalThis, {
+    right: 'riight',
+    left: 'lleft',
+  })
+
   .hide()
   .setColor('red')
-  .setWidth(4)
+  .setWidth(8)
   .goto(0, 0)
   .setAngle(0)
   .forward(100)
